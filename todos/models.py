8aebf63bfae7f34ff41e8ada383404b2fd1d5302
minki <wer153@naver.com>
2022-11-27 16:10:58 +0900

    ADD: Todo model

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,15 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    class Status(models.TextChoices):
+        TODO = 'TODO', 'TODO'
+        DONE = 'DONE', 'DONE'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=20)
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=Status.choices)
+
