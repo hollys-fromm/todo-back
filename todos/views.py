901254375cce78766f2282df203e3d9600b20d28
minki <wer153@naver.com>
2022-11-27 19:54:45 +0900

    CHG: use modelviewset

--- todos/views.py
+++ todos/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.permissions import AllowAny
 from rest_framework.serializers import ModelSerializer
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from todos.models import Todo
 
@@ -14,7 +14,7 @@ class TodoSerializer(ModelSerializer):
         fields = ('id', 'title', 'detail', 'status')
 
 
-class TodoViewSet(ViewSet):
+class TodoViewSet(ModelViewSet):
     queryset = Todo.objects
     serializer_class = TodoSerializer
     permission_classes = (AllowAny,)
