aa558004b18b48ac9bf250d897079fabbd4b37cd
minki <wer153@naver.com>
2022-12-04 20:12:51 +0900

    ADD: UserManager

---
+++ users/managers.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class UserManager(models.Manager):
+    def _create_user(self, email, password, **extra_fields):
+        """Creates and saves a User with the given email and password."""
+        if not email:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self,  email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+    @classmethod
+    def normalize_email(cls, email):
+        """
+        Normalize the address by lowercasing the domain part of the email
+        address.
+        """
+        email = email or ''
+        try:
+            email_name, domain_part = email.strip().rsplit('@', 1)
+        except ValueError:
+            pass
+        else:
+            email = '@'.join([email_name, domain_part.lower()])
+        return email
+
+    def get_by_natural_key(self, email):
+        return self.get(email=email)
--- users/models.py
+++ users/models.py
@@ -3,9 +3,13 @@ import uuid
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
+from users.managers import UserManager
+
 
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField('email', blank=True, unique=True)
+
+    objects = UserManager()
