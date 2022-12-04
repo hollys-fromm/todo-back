9a580042d6801285c618893291562a31a32e20af
minki <wer153@naver.com>
2022-12-04 20:01:54 +0900

    ADD: User Model

--- todo/settings.py
+++ todo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos'
+    'todos',
+    'users',
 ]
 
 DJANGO_APPS = [
@@ -50,7 +51,7 @@ DJANGO_APPS = [
 ]
 INSTALLED_APPS.extend(DJANGO_APPS)
 
-CORS_ALLOWED_ORIGINS = ['*']
+# CORS_ALLOWED_ORIGINS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
@@ -137,3 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+import uuid
+
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    USERNAME_FIELD = 'email'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField('email', blank=True, unique=True)
