e9755eb861c7674662f24bca7949fb6bcbf4f563
minki <wer153@naver.com>
2022-12-04 21:27:29 +0900

    CHG: filter by user

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
 
@@ -17,5 +17,8 @@ class TodoSerializer(ModelSerializer):
 class TodoViewSet(ModelViewSet):
     queryset = Todo.objects
     serializer_class = TodoSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = (IsAuthenticated,)
     lookup_field = 'id'
+
+    def get_queryset(self):
+        self.queryset.objects.filter(user=self.request.user)
